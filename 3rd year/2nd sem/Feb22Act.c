4961015fde41de40e399028a8fcd2e8d3294c2a9
keylperez <perez.williamcaleb@gmail.com>
2023-02-22 16:48:00 +0800

    done with comments

--- 3rd year/2nd sem/Feb22Act.c
+++ 3rd year/2nd sem/Feb22Act.c
@@ -32,7 +32,7 @@ LinkedList *sortCourse(ArrayList *AL, char string[8]);
 
 int main()
 {
-    ArrayList *AL = (ArrayList *)malloc(sizeof(ArrayList));
+    ArrayList *AL = (ArrayList *)malloc(sizeof(ArrayList)); // create the arraylist implemetation
     AL->count = 0;
     insertArr(AL, "Matugas", 'A', "Matugas", 18104944, "BSCS", 3);
     insertArr(AL, "Perez", 'B', "Perez", 18104944, "BSCS", 3);
@@ -44,13 +44,12 @@ int main()
     insertArr(AL, "Woogie", 'I', "Woogie", 18104944, "BSCS", 3);
     displayArr(AL);
     LinkedList *L = sortCourse(AL, "BSCS");
-    //    display(AL);
     display(AL, &L);
     free(AL);
     return 0;
 }
 
-void insertArr(ArrayList *AL, char fn[24], char mn, char ln[16], unsigned int id, char course[8], int year)
+void insertArr(ArrayList *AL, char fn[24], char mn, char ln[16], unsigned int id, char course[8], int year) // populate array
 {
     strcpy(AL->stud[AL->count].FName, fn);
     AL->stud[AL->count].MI = mn;
@@ -63,43 +62,44 @@ void insertArr(ArrayList *AL, char fn[24], char mn, char ln[16], unsigned int id
 
 LinkedList *sortCourse(ArrayList *AL, char string[8])
 {
-    LinkedList head = NULL;
+    LinkedList head = NULL; // create the linked list head
     LinkedList *trav;
     int i = 0;
-    while (i < AL->count)
+    while (i < AL->count) // not a for loop so iteration is conditional
     {
-        if (strcmp(AL->stud[i].course, string) == 0)
+        if (strcmp(AL->stud[i].course, string) == 0) // if current index course matches string parameter
         {
 
-            for (trav = &head; *trav != NULL && strcmp((*trav)->stud.LName, AL->stud[i].LName) < 0; trav = &(*trav)->link)
+            for (trav = &head; *trav != NULL && strcmp((*trav)->stud.LName, AL->stud[i].LName) < 0; trav = &(*trav)->link) // traverse through list and stop when the next node is null or the next node's last name is greater than the current index's last name
             {
+                // printf("\n%s", (*trav)->stud.LName);
             }
 
-            LinkedList new_node = malloc(sizeof(struct node));
+            LinkedList new_node = malloc(sizeof(struct node)); // create new node
 
-            if (new_node != NULL)
+            if (new_node != NULL) // check if node allocation was successful
             {
-                new_node->stud = AL->stud[i];
-                new_node->link = *trav;
-                *trav = new_node;
-                //                 new_node->link = (*trav)->link;
-                //                 (*trav)->link = new_node;
-                memmove(AL->stud + i, AL->stud + (i + 1), sizeof(AL->stud[0]) * (AL->count - i));
-                AL->count--;
-                i = 0;
+                new_node->stud = AL->stud[i];                                                     // new node's data is the current array index's data
+                new_node->link = *trav;                                                           // new node's link is next node after current node
+                *trav = new_node;                                                                 //
+                memmove(AL->stud + i, AL->stud + (i + 1), sizeof(AL->stud[0]) * (AL->count - i)); // move the array elements after the current index to the left
+                // sizeof AL->stud[0] is the size of the first element of the array which is a node so the size takes a whole node instead of 1 bit of memory
+                AL->count--; // since an element has been removed, the count is decremented
+                i--;         // checks current index (was i = 0 but decrement makes code more effecient)
             }
         }
         else
         {
-            i++;
+            i++; // increment i if current index course does not match string parameter
         }
     }
-    return head;
+    return head; // return address of first node
 }
 
 void displayArr(ArrayList *AL)
 {
     int i;
+    printf("Initial Array List:");
     for (i = 0; i < AL->count; i++)
     {
         printf("\n%u - %s %c. %s, %s", AL->stud[i].ID, AL->stud[i].FName, AL->stud[i].MI, AL->stud[i].LName, AL->stud[i].course);
@@ -112,11 +112,12 @@ void display(ArrayList *AL, LinkedList *L)
 {
     int i;
     LinkedList trav;
+    printf("\nMutated Array List:");
     for (i = 0; i < AL->count; i++)
     {
         printf("\n%u - %s %c. %s, %s", AL->stud[i].ID, AL->stud[i].FName, AL->stud[i].MI, AL->stud[i].LName, AL->stud[i].course);
     }
-    printf("\n\n\n");
+    printf("\n\nCourse Linked List:");
     for (trav = *L; trav != NULL; trav = trav->link)
     {
         printf("\n%u - %s %c. %s, %s", trav->stud.ID, trav->stud.FName, trav->stud.MI, trav->stud.LName, trav->stud.course);
